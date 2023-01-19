c6cda2c9e39521f39c1f26569f1947acb22037f6
t50tne <cotne.mamaladze1996@gmail.com>
2023-01-19 22:40:58 +0400

    Add project files.

---
+++ 2222/Car.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPBasis_Cars
+{
+    class Car
+    {
+        // variable
+        private string model = "Unknown";
+        private int maxspeed;
+        private double cc;
+        private int horsePower;
+        private int cylinder;
+        public string getModel()
+        {
+            return model;
+        }
+        public void setModel(string model)
+        {
+            this.model = model;
+        }
+
+        public int GetHorsePower()
+        {
+            return horsePower;
+        }
+        public void SetHorsePower(int horsePower)
+        {
+            this.horsePower = horsePower;
+        }
+
+        // Propertties
+        public string Color { get; set; } = "Unknown";
+        public int Year { get; set; }
+        public double Acceleration { get; set; }
+
+        // Third Way
+        public int MaxSpeed
+        {
+            get
+            {
+                return maxspeed;
+            }
+
+            set
+            {
+                maxspeed = value;
+            }
+        }
+
+        public double CC
+        {
+            get
+            {
+                return cc;
+            }
+
+            set
+            {
+                cc = value;
+            }
+        }
+
+        public int Cylinder
+        {
+            get
+            {
+                return cylinder;
+            }
+            set
+            {
+                cylinder = value;
+            }
+        }
+
+        public void getFullDetails()
+        {
+            Console.WriteLine("Model: {0}", getModel());
+            Console.WriteLine("Color: {0}", Color);
+            Console.WriteLine("Year: {0}", Year);
+            Console.WriteLine("Max Speed: {0}", MaxSpeed);
+            Console.WriteLine("Engine: {0}", CC);
+            Console.WriteLine("Horse Power: {0}", GetHorsePower());
+            Console.WriteLine("Acceleration: 0 -> 100 km/h: {0}", Acceleration);
+            Console.WriteLine("Number Of Cylinders: {0}", Cylinder);
+            Console.WriteLine();
+        }
+    }
+}
---
+++ 2222/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Runtime.ConstrainedExecution;
+
+namespace OOPBasis_Cars
+{
+    class MyMain
+    {
+        static void Main()
+        {
+            Car Toyota = new Car();
+            Toyota.setModel("CHR");
+            Toyota.Color = "white";
+            Toyota.Year = 2018;
+            Toyota.MaxSpeed = 300;
+            Toyota.CC = 2.0;
+            Toyota.SetHorsePower(144);
+            Toyota.Acceleration = 10.9;
+            Toyota.Cylinder = 4;
+
+            Car Nissan = new Car();
+            Nissan.setModel("Juke");
+            Nissan.Color = "green";
+            Nissan.Year = 2011;
+            Nissan.MaxSpeed = 280;
+            Nissan.CC = 1.6;
+            Nissan.SetHorsePower(188);
+            Nissan.Acceleration = 10.6;
+            Nissan.Cylinder = 4;
+
+            Car Porsche = new Car();
+            Porsche.setModel("911 GT3");
+            Porsche.Color = "blue";
+            Porsche.Year = 2019;
+            Porsche.MaxSpeed = 318;
+            Porsche.CC = 4.0;
+            Porsche.SetHorsePower(500);
+            Porsche.Acceleration = 3.9;
+            Porsche.Cylinder = 6;
+
+            Car Pagani = new Car();
+            Pagani.setModel("Zonda R");
+            Pagani.Color = "black";
+            Pagani.Year = 2011;
+            Pagani.MaxSpeed = 350;
+            Pagani.CC = 6.0;
+            Pagani.SetHorsePower(800);
+            Pagani.Acceleration = 2.7;
+            Pagani.Cylinder = 12;
+
+            Nissan.getFullDetails();
+            Toyota.getFullDetails();
+            Porsche.getFullDetails();
+            Pagani.getFullDetails();
+        }
+
+    }
+}
